ecbe7b9a43821903d50dd03059fe552bca27ed3f
Mars <Mars@163.com>
2025-08-01 10:31:10 +0800

    third commit

--- src/main/java/com/mars/GitTest.java
+++ src/main/java/com/mars/GitTest.java
@@ -2,7 +2,8 @@ package com.mars;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
+        System.out.println("Hello Git3");
     }
 }
